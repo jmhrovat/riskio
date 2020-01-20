86a2a905b8deab17cc05b99b6908c0bb9d8e83f9
Jonathan Hrovat <jmhrovat@gmail.com>
2020-01-20 12:09:38 -0800

    Added coverage to setup

---
+++ riskio/base/tests/test_urls.py
@@ -0,0 +1,9 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from base.views import *
+
+class TestUrls(SimpleTestCase):
+
+    def test_index_url_is_resolved(self):
+        url = reverse('index')
+        self.assertEquals(resolve(url).func, index)
